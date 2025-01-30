b481e220115fe886ced8f45b1d2551ffd085861a
ololola-lab <daandmitrieva@edu.hse.ru>
2025-01-30 19:20:02 +0300

    expression

---
+++ expression/Add.java
@@ -0,0 +1,13 @@
+package expression;
+
+public class Add extends BinaryOperations{
+    public Add(MyExpression firstVariable, MyExpression secondVariable) {
+        super(firstVariable, secondVariable, "+");
+    }
+
+    @Override
+    public int operate(int firstVar, int secondVar) {
+        return firstVar + secondVar;
+    }
+}
+
---
+++ expression/Divide.java
@@ -0,0 +1,12 @@
+package expression;
+
+public class Divide extends BinaryOperations {
+    public Divide(MyExpression firstVariable, MyExpression secondVariable) {
+        super(firstVariable, secondVariable, "/");
+    }
+
+    @Override
+    public int operate(int firstVar, int secondVar) {
+        return firstVar / secondVar;
+    }
+}
